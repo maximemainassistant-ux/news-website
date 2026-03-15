3bcea3e1c52793581cf66efeaca945009fe17b43
Ashique <labibhussain0@gmail.comm>
2026-03-15 22:24:17 +0530

    fix: replace catch-all studio route with simple static page

--- app/studio/page.tsx
+++ app/studio/page.tsx
@@ -1,8 +1,8 @@
 'use client';
 
 import { NextStudio } from 'next-sanity/studio';
-import config from '../../../sanity.config';
+import config from '../../sanity.config';
 
-export default function StudioClient() {
+export default function StudioPage() {
     return <NextStudio config={config} />;
 }
