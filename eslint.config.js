11a07fb85fa44981a1761aad4e8a3f16ef56c803
Ubuntu <azureuser@openclaw1.tailc95434.ts.net>
2026-03-16 10:50:44 +0000

    chore(eslint): drop react plugin until flat compat stabilizes

--- eslint.config.js
+++ eslint.config.js
@@ -2,7 +2,6 @@
  * ESLint configuration migrated to eslint.config.js (ESLint v9+ flat config format)
  */
 import { FlatCompat } from '@eslint/eslintrc';
-import reactPlugin from 'eslint-plugin-react';
 import tsParser from '@typescript-eslint/parser';
 import tsPlugin from '@typescript-eslint/eslint-plugin';
 
@@ -10,10 +9,7 @@ const compat = new FlatCompat({
   baseDirectory: new URL('.', import.meta.url).pathname,
   recommendedConfig: 'eslint:recommended'
 });
-const sharedConfigs = compat.extends(
-  'plugin:react/recommended',
-  'plugin:@typescript-eslint/recommended'
-);
+const sharedConfigs = compat.extends('plugin:@typescript-eslint/recommended');
 
 export default [
   ...sharedConfigs,
@@ -29,11 +25,7 @@ export default [
       }
     },
     plugins: {
-      react: reactPlugin,
       '@typescript-eslint': tsPlugin
-    },
-    settings: {
-      react: { version: 'detect' }
     },
     rules: {
       // project-specific rules go here
