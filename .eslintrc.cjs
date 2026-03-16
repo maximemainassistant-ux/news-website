327d6bfbba9fd45a9f8be4aaf5bd7511018ce32a
Ubuntu <azureuser@openclaw1.tailc95434.ts.net>
2026-03-16 10:58:27 +0000

    chore(eslint): use CommonJS config for lint

---
+++ .eslintrc.cjs
@@ -0,0 +1,29 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2020: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  plugins: ['react', '@typescript-eslint'],
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
+  rules: {
+    'react/prop-types': 'off'
+  },
+  ignorePatterns: ['.next/**']
+};
